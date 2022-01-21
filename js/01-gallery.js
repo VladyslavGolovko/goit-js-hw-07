83136c242cf907023196e6e5e0ae492a870a6a98
VladyslavGolovko <86726358+VladyslavGolovko@users.noreply.github.com>
2022-01-21 21:16:55 +0200

    changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,12 +18,12 @@ function createGalleryCardsMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </div>`;
